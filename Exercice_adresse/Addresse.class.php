46d7921b71264bca17c69a086df1d4f6f6e1e77b
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-10-02 18:55:49 +0200

    initial commit addresse exercice

---
+++ Exercice_adresse/Addresse.class.php
@@ -0,0 +1,21 @@
+<?php
+class Addresse {
+    // Attributes
+    private $rue;
+    private $numero;
+    private $localite;
+    private $codePostale;
+    private $pays;   
+
+    // Constructeur
+    public function __construct($rue){
+        $this->rue = $rue;
+        $this->rue = $rue;
+        $this->rue = $rue;
+        $this->rue = $rue;
+        $this->rue = $rue;
+    }
+}
+
+
+?>
