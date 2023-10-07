c9317101c10a11db78ad8dc684c74bd8ebf2531b
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-10-07 11:15:56 +0200

    initial commit composer

---
+++ Exercice_Composer/index.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+// PSR-4 autoloading configured. Use "namespace Poo\ExempleComposer;" in src/
+// Include the Composer autoloader with: require 'vendor/autoload.php';
+?>
