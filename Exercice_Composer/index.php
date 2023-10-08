fcae59548d3b2e3ada8a36fceb6bc79640a47886
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-10-08 09:27:32 +0200

    adapting namespaces

--- Exercice_Composer/index.php
+++ Exercice_Composer/index.php
@@ -1,14 +1,10 @@
 <?php
 // Composer autoloader with: require 'vendor/autoload.php';
 require 'vendor/autoload.php';
+use Poo\ExempleComposer\manager\personneManager;
 
-// use FakerPHP to generate personnes
-// use the factory to create a Faker\Generator instance
-$faker = Faker\Factory::create();
-echo $faker->name();
-echo $faker->firstName();
-echo $faker->streetAddress();
-echo $faker->postcode();
-echo $faker->country();
-echo $faker->company();
+// affichage
+$newPersonne = new personneManager();
+$testPersonne = $newPersonne->create();
+echo $testPersonne;
 ?>
--- Exercice_Composer/src/manager/personneManager.php
+++ Exercice_Composer/src/manager/personneManager.php
@@ -1,12 +1,18 @@
 <?php
 // Defining a namespace
-//namespace Poo\ExempleComposer\manager;
+namespace Poo\ExempleComposer\manager;
 
 // Declaring the classes to be used
-//use Poo\ExempleComposer\entity\personne;
+use Poo\ExempleComposer\entity\personne;
+use faker;
 
 // generate fake people using faker: PersonneManager::create($nombre)
 class personneManager{
+    // constructor
+    public function __construct(){
+    }
+
+    // other functions
     public function create(){
         $faker = Faker\Factory::create();
         $faker->name();
@@ -17,7 +23,5 @@ class personneManager{
         $faker->company();
         return $faker;
     }
-
-
 }
 ?>
