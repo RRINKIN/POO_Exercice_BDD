0064bafb99492ca3478ef81d111610e769a2fc4d
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-10-08 22:44:50 +0200

    correcting crud

--- Exercice_Composer/index.php
+++ Exercice_Composer/index.php
@@ -13,32 +13,32 @@ $newPersonne = new personneManager($connexion);
 $testPersonne = $newPersonne->create($number);
 // Affichage de Faker
 echo "<table>";
-echo "<thead>";
-echo "<tr>";
-echo "<th>nom</th>";
-echo "<th>prénom</th>";
-echo "<th>adresse</th>";
-echo "<th>code postal</th>";
-echo "<th>pays</th>";
-echo "<th>société</th>";
-echo "</tr>";
-echo "</thead>";
-echo "<tbody>";
-/**
- * @var personne $personne
- */
-foreach ($testPersonne as $personne) {
-    echo "<tr>";
-    echo "<th>".$personne->getName()."</th>";
-    echo "<th>".$personne->getPrenom()."</th>";
-    echo "<th>".$personne->getAdresse()."</th>";
-    echo "<th>".$personne->getPostalCode()."</th>";
-    echo "<th>".$personne->getPays()."</th>";
-    echo "<th>".$personne->getSociete()."</th>";
-    echo "</tr>";
-    //$newPersonne->insert($personne);
-}
-echo "</tbody>";
+    echo "<thead>";
+        echo "<tr>";
+            echo "<th>nom</th>";
+            echo "<th>prénom</th>";
+            echo "<th>adresse</th>";
+            echo "<th>code postal</th>";
+            echo "<th>pays</th>";
+            echo "<th>société</th>";
+        echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    /**
+     * @var personne $personne
+     */
+    foreach ($testPersonne as $personne) {
+        echo "<tr>";
+            echo "<th>".$personne->getName()."</th>";
+            echo "<th>".$personne->getPrenom()."</th>";
+            echo "<th>".$personne->getAdresse()."</th>";
+            echo "<th>".$personne->getPostalCode()."</th>";
+            echo "<th>".$personne->getPays()."</th>";
+            echo "<th>".$personne->getSociete()."</th>";
+        echo "</tr>";
+        //$newPersonne->insert($personne);
+    }
+    echo "</tbody>";
 echo "</table>";
 $newPersonne->readAll();
 $newPersonne->read(1);
